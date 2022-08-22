34e6355e22e30b46e975ea3eb09cf1e596cbe871
Zvonarev23 <zvonarev.evgeniy@gmail.com>
2022-08-22 20:53:11 +0300

    lesson-10: added mobile-menu

--- js/main.js
+++ js/main.js
@@ -22,3 +22,11 @@ const swiper2 = new Swiper('.reviews-swiper', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+var menuBtn = document.querySelector('.burger-menu');
+menuBtn.addEventListener('click', function () {
+
+  document.querySelector('.nav').classList.toggle('nav_mobile_visible')
+  document.querySelector('body').classList.toggle('lock')
+
+}); 
